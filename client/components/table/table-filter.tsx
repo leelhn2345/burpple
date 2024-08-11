1dc94080a39e486137cb31682ea405962a650e4a
Nelson Lee <leelhn2345@outlook.com>
2024-08-11 21:16:38 +0800

    reset page if search filter changed

--- client/components/table/table-filter.tsx
+++ client/components/table/table-filter.tsx
@@ -1,7 +1,7 @@
 import { Input } from "@/components/ui/input";
 import { TableQuery } from "@/lib/query-params/table";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useCallback, useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { useDebounce } from "use-debounce";
 
 export function TableFilter() {
@@ -9,28 +9,13 @@ export function TableFilter() {
   const [searchValue] = useDebounce(value, 500);
 
   const router = useRouter();
-  const searchParams = useSearchParams();
   const pathname = usePathname();
 
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams.toString());
-      if (!value || value.length < 2) {
-        params.delete(name);
-      } else {
-        params.set(name, value);
-      }
-
-      return params.toString();
-    },
-    [searchParams],
-  );
-
   useEffect(() => {
-    router.push(
-      pathname + "?" + createQueryString(TableQuery.SEARCH, searchValue),
-    );
-  }, [router, searchValue, createQueryString, pathname]);
+    const searchParams = new URLSearchParams();
+    searchParams.set(TableQuery.SEARCH, searchValue);
+    router.push(pathname + "?" + searchParams);
+  }, [router, searchValue, pathname]);
 
   return (
     <Input
